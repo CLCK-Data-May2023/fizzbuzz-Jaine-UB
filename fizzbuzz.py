5925e7e415f783b10b6389675f769871ac4ec117
Jaine <114710646+Jaine-UB@users.noreply.github.com>
2023-07-20 19:07:08 -0400

    Update fizzbuzz.py
    first try.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,12 @@
-# add your code here
+# Print the numbers until 100. The numbers divisivble by 3 show Fizz, the numbers divisivble by 5 show Buzz, the numbers divisivble by 15 show FizzBuzz.
 
+for i in range(1,101):
+    if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+    elif i % 3 == 0:
+            print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+        
+    else:
+        print(i)
